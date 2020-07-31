c18427c7e6f244d1f1eb2dbef0363d3252c3cd0e
diego-034 <diego-034>
2020-07-31 07:59:46 -0500

    Update last

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,15 +1,11 @@
 /* request to API */
 function getData() {
     try {
-        fetch("https://coinpaprika.com/ajax/coins/1?section=social&all=false&expand=social%2Cprice_stats&sort%5Bsort%5D=index&sort%5Bsortorder%5D=asc&tagID=&filters%5Bcoins%5D=true&filters%5Btokens%5D=true&currency=usd", {
-                "headers": {
-                    "Access-Control-Allow-Origin": "*"
-                }
-            })
+        fetch("https://api.coinpaprika.com/v1/tickers")
             .then((response) => response.json())
             .then((response) => {
                 /* filter th array */
-                data = response.data;
+                data = response.filter((elem) => elem.rank > 0 && elem.rank <= 100);
                 data.sort((a, b) => a.rank - b.rank);
                 /* validation */
                 if (data[0] == null) {
